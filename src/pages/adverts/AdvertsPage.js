15e0be2d6de567097b68df150294bb9857e47f68
Gabriela <grabyaprende@gmail.com>
2023-12-05 19:50:01 +0100

    Cambios realizados para la pagina publica y privada en advert page e index html

--- src/pages/adverts/AdvertsPage.js
+++ src/pages/adverts/AdvertsPage.js
@@ -1,26 +1,94 @@
 //importamos la función getLatestAdverts para hacer la llamada al service.
 import { useEffect, useState } from "react";
 import { getLatestAdverts } from "./service";
+import { getTags } from '../newAdvertForm/service';
 
 function AdvertsPage()  {
     const [adverts, setAdverts] = useState([]) //aqui definimos el estado donde se almacena los anuncios que nos llegue de la peticion
+    const [allTags, setAllTags] = useState([]); // allTags seran todos los tags que vengan desde la API
+    const [type, setType] = useState('');
+    const [selectedTag, setSelectedTag] = useState('');
+    
+
+    // Solo la primera vez que el componente renderice, vamos a traernos los tags de la API
+    useEffect(() => {
+        getTags()
+            .then((tags) => setAllTags(() => {
+                return tags;
+            }))
+            .catch((err) => console.error(err));
+    }, []) // <- este [] es la dependencia que necesitamos para renderizarlo solo la primera vez
     
     //para que no sea un bucle infinito usamos useEffect y le pasamos dos parametros
     useEffect(() => {
         //aquí hacemos la llamada al método get(service)que devuelve una promesa o el resultado del get
-        getLatestAdverts().then(adverts => setAdverts(adverts) ).catch(err => console.error(err))//luego definimos un estado para que se guarden los anuncios
-    }, [])
+        getLatestAdverts(type, selectedTag)
+            .then(adverts => setAdverts(adverts) )
+            .catch(err => console.error(err)) //luego definimos un estado para que se guarden los anuncios
+    }, [type, selectedTag])
+    
+    const filterByType = (event) => {
+        const typeFromEvent = event.target.value;
+        setType(typeFromEvent)
+    }
+
+    const filterByTag = (event) => {
+        const tagFromEvent = event.target.value;
+        setSelectedTag(tagFromEvent)
+    }
     
     return (
-        <div>
-            <ul>
-                {adverts.map(advert => (
-                <li key={advert.id}>
-                    <span>{advert.content}</span>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        // La clase container en PicoCSS hace que el contenido tenga margenes a la izquierda y derecha
+        <section className="container"> 
+            <h1>Anuncios</h1>
+
+            <label htmlFor="select-type">Tipo de anuncio</label>
+            <select id="select-type" name="select-type" onChange={filterByType}>
+                <option value="">Todos</option>
+                <option value="compra">Compra</option>
+                <option value="venta">Venta</option>
+            </select>
+
+            <label htmlFor="select-tag">Tipo de anuncio</label>
+            <select id="select-tag" name="select-tag" onChange={filterByTag}>
+                <option value="">Todos</option>
+                {
+                    allTags.map((tag) => {
+                        return <option key={`option-${tag}`} value={tag}>{tag}</option>
+                    })
+                }
+            </select>
+
+            {/* La clase "grid" en PicoCSS nos hace un grid automaticamente */}
+            <div className="grid">
+                {adverts.length === 0
+                    ? (
+                        // Cuando no haya anuncios, ponemos esto
+                        <article>
+                            <div>No hay aún anuncios, ¿Quieres crear uno?</div>
+                            <a role="button" href={'/adverts/new'}>Crea tu anuncio</a>
+                        </article>
+                    )
+                    : (
+                        /* Aqui vamos a recorrer todos nuestros anuncios */
+                        adverts.map((advert) => {
+                            return (
+                                // article en PicoCss nos crea una "tarjeta"
+                                <article key={advert.id}>
+                                    <h2>{advert.name}</h2>
+                                    <img src={advert.photo} alt={advert.name} />
+                                    <div>
+                                        Precio: {advert.price} €
+                                    </div>
+                                    <div>{ advert.sale === true ? 'Venta' : 'Compra'  }</div>
+                                    {/* Ponemos la direccion dinamica, dependiendo del id de advert */}
+                                    <a role="button" href={`/adverts/${advert.id}`}>Ir al anuncio</a>
+                                </article>
+                        )})
+                    )
+                }
+            </div>
+        </section>
     );
 }
 
--- src/pages/adverts/service.js
+++ src/pages/adverts/service.js
@@ -3,18 +3,35 @@ import client from "../../api/cliente";
 
 const advertURL = '/api/v1/adverts'
 
+const getAdvetUrl = (type, tag) => {
+    // Inicializamos url con advertUrl
+    let url = advertURL;
+
+    if (type) {
+        // Si tipo es compra, iniciamos los parametros con ?
+        if (type === 'compra') {
+            url = `${url}?sale=false`;
+        }
+        else if (type === 'venta') {
+            url = `${url}?sale=true`;
+        }
+    }
+
+    if (tag) {
+        // Si hay type, empezara por el prefijo & y sino hay iniciamos los parametros con ?
+        if (type) {
+            url = `${url}&tags=${tag}`
+        } else {
+            url = `${url}?tags=${tag}`
+        }
+    }
+
+    return(url)
+}
+
 //otro método para que me devuelva la promesa
-export const getLatestAdverts = async ( ) => {
-    const tupi = await client.post("/api/auth/login", {
-        "email": "victor@tupi.com",
-        "password": "12345678"
-    })
-
-    console.log({ tupi })
-
-    await client.get("/api/auth/me")
-
-    return client.get(advertURL);
+export const getLatestAdverts = (type, tag) => {
+    return client.get(getAdvetUrl(type, tag));
 };
 
 //luego hacer la llamada desde mi componente(fichero)
